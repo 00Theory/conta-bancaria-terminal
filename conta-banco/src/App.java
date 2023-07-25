7d92be34bbf32492742c1e38678189b3174b3659
00Theory <marco200002010@outlook.com>
2023-07-25 09:25:04 -0300

    projeto conta banco

--- conta-banco/src/App.java
+++ conta-banco/src/App.java
@@ -1,8 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
-        int a = 0;
-        System.out.println(a);
     }
 }
